6639c3487f6a576c1e19001e50f39f206043861d
Alex Dorey <dorey415@gmail.com>
2015-09-28 16:08:31 -0400

    Add the form themes into the <body> for ez css customizations

--- src/js/Form.js
+++ src/js/Form.js
@@ -64,6 +64,24 @@ define( function( require, exports, module ) {
 
             loadErrors = loadErrors.concat( form.init() );
 
+            (function($body){
+                /*
+                patch to get form themes into a document.body element's data-* attrs
+                e.g. a form with "theme-unga-grid" will be:
+                    <body data-form-theme-unga-grid="true">
+                and can be matched with selector:
+                    body[data-form-theme-unga-grid] { ... }
+                */
+                var $body = $(document.body),
+                    domClassList = form.$.get(0).classList,
+                    classArray = Array.prototype.concat.apply([], domClassList);
+                classArray.filter(function(kls){
+                    return kls.match(/theme-*/);
+                }).forEach(function(theme){
+                    $body.attr('data-form-'+theme, "true");
+                });
+            })()
+
             if ( window.scrollTo ) {
                 window.scrollTo( 0, 0 );
             }
